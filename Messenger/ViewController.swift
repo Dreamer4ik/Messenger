1a6d3e4c04297b55503efd517e8f40cada408e08
Ivan Potapenko <dreamer4ik@gmail.com>
2021-10-21 11:05:31 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ivan Potapenko on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
